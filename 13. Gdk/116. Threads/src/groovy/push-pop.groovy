4c6e240ae0f836e7ea354021298d41ce04ea69cd
Nibi <nibi.amdukia@gmail.com>
2018-08-30 15:50:23 +0200

    GDK

---
+++ 13. Gdk/116. Threads/src/groovy/push-pop.groovy
@@ -0,0 +1,21 @@
+package groovy
+
+List numbers = []
+
+// push
+10.times {
+    println "push\t: ${it}"
+    numbers << it
+}
+
+println numbers
+
+//pop
+for( i in 9..0 ) {
+    println "pop\t: ${i}"
+    numbers.pop()
+}
+
+println numbers
+
+// but what if we wanted to do this concurrently
